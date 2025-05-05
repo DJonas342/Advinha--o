8e373fb7666656f96885971b189af35e6457d4f3
Djota_ <dariojonasdrx@gmail.com>
2025-05-05 17:33:04 -0300

    atualizando funcionalidades

--- script.js
+++ script.js
@@ -9,13 +9,28 @@
 //9. Exibir o número de tentativas restantes.
 //10. Se o jogador atingir o número máximo de tentativas, o jogo termina com a mensagem "Você perdeu! O número secreto era X".
 
+// Funções utilitarias
 const obterElemento = (id) => document.getElementById(id);
+let tentativas = 15;
+let numberSecret = Math.floor(Math.random() * 101);
+
+//Palpite e validação de valor
 const valorInformado = () => parseInt(obterElemento("palpite").value);
 const valorInvalido = (valorInformado) => isNaN(valorInformado) || valorInformado < 0 || valorInformado > 100;
-const numberSecret = Math.floor(Math.random() * 101);
-let tentativas = 15;
 
-console.log("Número secreto:", numeroSecreto); //teste de numero secreto
+
+
+// função de reiniciar jogo após vitoria ou derrota
+function resetarJogo() {
+    tentativas = 15;
+    numberSecret = Math.floor(Math.random() * 101);
+    obterElemento("dica").textContent = "";
+    obterElemento("chances").textContent = "Você tem 15 chances";
+    obterElemento("palpite").value = "";
+    console.log("Novo número secreto:", numberSecret); // só pra debug
+}
+
+console.log("Número secreto:", numberSecret); //teste de numero secreto
 
 function chutar(){
     const valor = valorInformado()
@@ -25,16 +40,21 @@ function chutar(){
         return;
     }
 
-    if (valor === numeroSecreto) {
+    if (valor === numberSecret) {
         obterElemento("dica").textContent = "🎉 Você ganhou!";
-    } else {
+        obterElemento("chances").textContent = "O jogo sera reiniciado em 10 segundos"
+        setTimeout(resetarJogo, 10000); // reinicia em 10 segundos
+        return;
+        } else {
         tentativas--;
         obterElemento("chances").textContent = "Você tem " + tentativas + " chances";
 
         if (tentativas <= 0) {
-            obterElemento("dica").textContent = "💀 Fim de jogo. O número era " + numeroSecreto;
-        } else {
-            obterElemento("dica").textContent = valor < numeroSecreto ? "🔼 Tente um número maior" : "🔽 Tente um número menor";
+            obterElemento("dica").textContent = "💀 Fim de jogo. O número era " + numberSecret;
+            obterElemento("chances").textContent = "O jogo sera reiniciado em 10 segundos"
+            setTimeout(resetarJogo, 10000); // reinicia em 10 segundos
+            return;} else {
+            obterElemento("dica").textContent = valor < numberSecret ? "🔼 Tente um número maior" : "🔽 Tente um número menor";
         }
     }
 }
